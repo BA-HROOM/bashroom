4d680bd3dce2e41abb3f7963e8a09ff0d656ddfc
Yohann Vernhes <0pdlna3@gmail.com>
2020-12-03 17:35:21 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8"/>
+  <title>BA$HROOM</title>
+</head>
+<body>
+  <h1>Auto Desployed app</h1>
+  <p>Tous ce que vous pushez, sera mis en ligne automatiquement ! enjoi !</p>
+</body>
+</html>
